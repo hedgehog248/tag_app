b73f61cecb8a9840cf0c9e9ce95c244e831dc0ab
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-04-22 00:08:16 +0900

    データを更新する処理

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -2,8 +2,10 @@ class PostForm
   include ActiveModel::Model
 
   #PostFormクラスのオブジェクトがPostモデルの属性を扱えるようにする
-  attr_accessor :text, :image
-
+  attr_accessor(
+    :text, :image,
+    :id, :created_at, :updated_at
+   )
   with_options presence: true do
     validates :text
     validates :image
@@ -11,5 +13,9 @@ class PostForm
 
   def save
     Post.create(text: text, image: image)
+  end
+
+  def update(params, post)
+    post.update(params)
   end
 end
